2d47f3afe018f3522dc0f912fa3cb1cd86fa7835
Masselot H <101673780+Badraf69@users.noreply.github.com>
2025-02-21 08:00:47 +0100

    🐦‍🔥MLT- uniformisation style page

--- Annuaire/ViewModel/MainViewModel.cs
+++ Annuaire/ViewModel/MainViewModel.cs
@@ -4,24 +4,31 @@ using System.Windows.Input;
 
 namespace Annuaire.Views;
 
-public class MainViewModel 
+public class MainViewModel : INotifyPropertyChanged
 {
     public MainViewModel()
     {
-        NavigateToListEmployee = App.NavigationVM.NavigateToListEmployeeCommand;
+        NavigateToListEmployeeCommand = App.NavigationVM.NavigateToListEmployeeCommand;
         //NavigateToAddEmployeeCommand = App.NavigationVM.NavigateToAddEmployeeCommand;
         NavigateToListServiceCommand = App.NavigationVM.NavigateToListServiceCommand;
         //NavigateToAddServiceCommand = App.NavigationVM.NavigateToAddServiceCommand;
         NavigateToListSiteCommand = App.NavigationVM.NavigateToListSiteCommand;
         //NavigateToAddSiteCommand = App.NavigationVM.NavigateToAddSiteCommand;
 
-    }
-    public ICommand NavigateToListEmployee { get; }
-    public ICommand NavigateToAddEmployeeCommand { get; }
-    public ICommand NavigateToListServiceCommand { get; }
-    public ICommand NavigateToAddServiceCommand { get; }
-    public ICommand NavigateToListSiteCommand { get; }
-    public ICommand NavigateToAddSiteCommand { get; }
-    
 
+    }
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public ICommand NavigateToListEmployeeCommand { get; }
+    //public ICommand NavigateToAddEmployeeCommand { get; }
+    public ICommand NavigateToListServiceCommand { get; }
+    //public ICommand NavigateToAddServiceCommand { get; }
+    public ICommand NavigateToListSiteCommand { get; }
+    //public ICommand NavigateToAddSiteCommand { get; }
+
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
